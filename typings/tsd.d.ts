4cf615b5cad7b34899d2ff7196196ee87eeca0c2
Asher <asher.barak@gmail.com>
2015-06-21 19:07:39 +0300

    typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,9 @@
+﻿
+/// <reference path="angular2/angular2.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="rx/rx.d.ts" />
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="rx/rx-lite.d.ts" />
+/// <reference path="urijs/URIjs.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
