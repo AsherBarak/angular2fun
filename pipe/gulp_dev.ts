5aaa46e0a83afb07e08113a19128d7eb6f194b40
Asher <asher.barak@gmail.com>
2015-06-22 09:49:06 +0300

    comment es6 from node.d.ts

--- pipe/gulp_dev.ts
+++ pipe/gulp_dev.ts
@@ -1,21 +1,21 @@
 
 /// <reference path="../typings/tsd.d.ts" />
 import {createServer } from 'http';
-import open=require('open');
+import open = require('open');
 import gulp = require('gulp');
 
-const PORT=2020;
+const PORT:number = 2020;
 
 export module Pipe.Dev {
     export function runDev() {
         console.log("hello world!!!!!!!!!!!!!!!");
-        var server=createServer((req,res)=>{
-                res.writeHead(200, {"Content-Type": "text/plain"});
-                res.end("Hello World\n");
+        var server = createServer((req, res) => {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("Hello World\n");
         });
-        
-        server.listen(PORT,()=>{open('http://localhost:' + PORT)});
-        console.log("listenting at "+PORT);
+
+        server.listen(PORT, () => { open('http://localhost:' + PORT) });
+        console.log("listenting at " + PORT);
     }
 }
 
